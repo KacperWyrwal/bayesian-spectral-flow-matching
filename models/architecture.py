274df9b226c9b11fcbaba56d3db23b52b3a5b0b0
Mohamed Bouhamed <mohamed.bouhamed19@student-cs.fr>
2026-05-25 20:11:42 +0200

    change dummy architecture

--- models/architecture.py
+++ models/architecture.py
@@ -11,10 +11,11 @@ class dummy_mlp(torch.nn.Module):
         self.model_config = model_config
         # do not mutate the passed config object; compute internal input dim
         in_dim = model_config.input_dim + (1 if model_config.enable_time else 0)
-        self.linear1 = torch.nn.Linear(in_dim, model_config.intermediate_dim)
-        self.linear2 = torch.nn.Linear(model_config.intermediate_dim, model_config.intermediate_dim)
-        self.linear3 = torch.nn.Linear(model_config.intermediate_dim, model_config.output_dim)
-        self.linear4 = torch.nn.Linear(model_config.output_dim, 1)
+        self.linear1 = torch.nn.Linear(in_dim, 2*model_config.intermediate_dim)
+        self.linear2 = torch.nn.Linear(2*model_config.intermediate_dim, model_config.intermediate_dim)
+        self.linear3 = torch.nn.Linear(model_config.intermediate_dim, model_config.intermediate_dim)
+        self.linear4 = torch.nn.Linear(model_config.intermediate_dim, 2*model_config.intermediate_dim)
+        self.linear5 = torch.nn.Linear(2*model_config.intermediate_dim, model_config.output_dim)
 
     def forward(self, x, t=None):
         if t is not None and self.model_config.enable_time:
@@ -26,4 +27,6 @@ class dummy_mlp(torch.nn.Module):
         x = self.linear3(x)
         x = torch.selu(x)
         x = self.linear4(x)
+        x = torch.selu(x)
+        x = self.linear5(x)
         return x
